ad67952264d9e4ce3146973121973df4492f46cf
trishamasbate <trisha.masbate@gmail.com>
2024-06-07 13:12:30 -0600

    Added code to js files.

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
 // Global Variables
 
+// Include packages required for this application
+const { prompt } = require('inquirer');
+const { writeFile } = require('fs');
+const { promisify } = require('util');
 
+// Import questions.js 
+const questions = require('./library/questions')
 
-// Functions
+// Import template.js
+const generateREADME = require('./library/template')
+
+// Wrap fs.writeFile inside util.promisify
+const writeFileAsync = promisify(writeFile);
+
+/* FUNCTIONS ==================================================================================================== */
+
+// Inital welcome message
+console.log('Welcome to Github README generator!');
+
+//Create a function to prompt user questions
+function promptUser() {
+	return prompt(questions);
+}
+
+// Use async... await
+async function init() {
+  //try ... catch block
+	try {
+    const answers = await promptUser();
+    
+    console.log(JSON.stringify(answers, null, '\t'));
+
+		const readme = generateREADME(answers);
+
+    await writeFileAsync('README.md', readme);
+
+		console.log('Successfully wrote to README.md');
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+init();
